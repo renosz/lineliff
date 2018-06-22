b5427a0e5e901452fb4825b23d3503c6909d187b
orayatiew <oraya.stiew@gmail.com>
2018-06-22 13:50:25 +0700

    app

---
+++ app.js
@@ -0,0 +1,40 @@
+// Reply with two static messages
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {PZYo9CQnLFhf4XX+wX6SaN2bF0mjCoYjeR60qNXdskBVMERB/H8/qbAF1NsqHoZUmZ/FyMW4/RD6JsXzVZzvzFx7DJPYNEkM4hF3o0/aE4PRrQi7I6J1uLw9wa8azWvQtKwYwGj+TPuTNUNaZFyAowdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+        
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
